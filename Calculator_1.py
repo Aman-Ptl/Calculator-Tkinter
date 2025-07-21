9c96f5dc4054021e76a774abcdf65b503c7de7bf
Ptl-aman <Ptlaman2711@gmail.com>
2025-07-21 13:41:23 +0530

    adding function for operators

--- Calculator_1.py
+++ Calculator_1.py
@@ -38,23 +38,92 @@ b=Button(window, text="8", width=12, command=lambda:click(8))
 b.place(x=80,y=180)
 b=Button(window, text="9", width=12, command=lambda:click(9))
 b.place(x=150,y=180)
-b=Button(window, text="-", width=12, command=lambda:click(1))
-b.place(x=10,y=240)
+
 b=Button(window, text="0", width=12, command=lambda:click(0))
 b.place(x=80,y=240)
-b=Button(window, text="+", width=12, command=lambda:click(1))
+
+# OPERATORS:
+def sub():
+    n1=e.get()
+    global math
+    math= "Substraction"
+    global i
+    i=int(n1)
+    e.delete(0,END)
+
+b=Button(window, text="-", width=12, command = sub)
+b.place(x=10,y=240)
+def add():
+    n1=e.get()
+    global math
+    math = "Addition"
+    global i
+    i=int(n1)
+    e.delete(0,END)
+b=Button(window, text="+", width=12, command= add)
 b.place(x=150,y=240)
-b=Button(window, text="//", width=12, command=lambda:click(1))
+def div():
+    n1=e.get()
+    global math
+    math = "Division"
+    global i
+    i=int(n1)
+    e.delete(0,END)
+b=Button(window, text="/", width=12, command= div)
 b.place(x=10,y=300)
-b=Button(window, text="*", width=12, command=lambda:click(1))
+def mult():
+    n1=e.get()
+    global math
+    math = "Multiplication"
+    global i
+    i=int(n1)
+    e.delete(0,END)
+b=Button(window, text="*", width=12, command=mult)
 b.place(x=80,y=300)
-b=Button(window, text="%", width=12, command=lambda:click(1))
+
+def modulus():
+    n1=e.get()
+    global math
+    math = "Modulus"
+    global i
+    i=int(n1)
+    e.delete(0,END)
+b=Button(window, text="%", width=12, command=modulus)
 b.place(x=150,y=300)
-b=Button(window, text="pow", width=12, command=lambda:click(1))
+
+def pow():
+    n1=e.get()
+    global math
+    math = "Power"
+    global i
+    i=int(n1)
+    e.delete(0,END)
+b=Button(window, text="pow", width=12, command=pow)
 b.place(x=10,y=360)
-b=Button(window, text="=", width=12, command=lambda:click(1))
+
+def equal():
+    n2=int(e.get())
+    e.delete(0,END)
+    if math=="Addition":
+        e.insert(0,i + n2)
+    elif math=="Substraction":
+        e.insert(0,i - n2)
+    elif math == "Multiplication":
+        e.insert(0, i * n2)
+    elif math == "Division":
+        e.insert(0,i / n2)
+    elif math == "Modulus":
+        e.insert(0, i % n2)
+    elif math == "Power":
+        e.insert(0, i ** n2)
+
+b=Button(window, text="=", width=12, command=equal)
 b.place(x=80,y=360)
-b=Button(window, text="clear", width=12, command=lambda:click(1))
+
+def clear():
+    e.delete(0, END)
+
+b=Button(window, text="clear", width=12, command= clear)
 b.place(x=150,y=360)
 #Step 4: main loop
 mainloop()
